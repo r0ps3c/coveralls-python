94a7e6df90fd8de226f4db2420e35c6ac639d8ba
r0ps3c <rudolph+gh@faex.net>
2020-11-05 23:08:29 -0500

    Fix previous commit

--- coveralls/api.py
+++ coveralls/api.py
@@ -208,7 +208,7 @@ class Coveralls:
         # check and adjust/resubmit if submission looks like it 
         # failed due to resubmission (non-unique)
         if response.status_code == 422:
-            if not (service_number in self.config and self.config['service_number']):
+            if not ('service_number' in self.config and self.config['service_number']):
                 self.config['service_number'] = random.randint(0,sys.maxsize)
             self.config['service_job_id']='{}-{}'.format(self.config['service_job_id'],self.config['service_number'])
         
